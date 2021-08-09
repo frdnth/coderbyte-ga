1b68987d8a0bd54009f4db013446ec1797c5cf67
frdnth <fardian.thofani@gmail.com>
2021-08-09 23:45:38 +0700

    init

---
+++ tryout-30july/01-capitalize.js
@@ -0,0 +1,10 @@
+/* String Challenge
+Have the function StringChallenge(str) 
+take the str parameter being passed 
+and capitalize the first letter of each word. 
+Words will be separated by only one space.
+Examples
+Input: "hello world"
+Output: Hello World
+Input: "i ran there"
+Output: I Ran There */
