c63a3033829370693bf26545a1842e345b45e106
frdnth <fardian.thofani@gmail.com>
2021-08-10 22:56:43 +0700

    did the 01

--- tryout-30july/01-capitalize.js
+++ tryout-30july/01-capitalize.js
@@ -8,3 +8,16 @@ Input: "hello world"
 Output: Hello World
 Input: "i ran there"
 Output: I Ran There */
+
+function capitalize(str) {
+	let arr = str.split(" ")
+	let newArr = []
+
+	for(i=0; i<arr.length; i++) {
+		newArr.push(arr[i].replace(arr[i][0], arr[i][0].toUpperCase()))
+	}
+	return newArr.join(" ")
+}
+
+console.log(capitalize("hello world"));
+console.log(capitalize("lorem ipsum dolor sit amet, consectetur adipiscing elit"))
